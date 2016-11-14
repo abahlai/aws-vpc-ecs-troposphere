bfaec2dabe50b4e48aaa131928cba126b74cc857
Andrew Bahlai <nikotinik@hotmail.com>
2016-11-14 16:21:56 +0200

    changes for subnet

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import troposphere.ec2 as ec2
 
 cidr_vpc = '10.0.0.0/16'
 cidr_subnet_pub = '10.0.0.0/24'
-cidr_subnet_priv = '10.0.1.0/24'
 
 template = Template()
 
@@ -11,17 +10,6 @@ vpcName = template.add_parameter(Parameter(
     'vpcName',
     Type="String"
 ))
-
-natKeyName = template.add_parameter(Parameter(
-    'natKeyName',
-    Type="String"
-))
-
-natIP = template.add_parameter(Parameter(
-    'natIP',
-    Type="String"
-))
-
 
 vpc = template.add_resource(ec2.VPC(
     'myVpc',
@@ -61,16 +49,6 @@ template.add_resource(ec2.SecurityGroupEgress(
     CidrIp='0.0.0.0/0'
 ))
 
-internetGateway = template.add_resource(ec2.InternetGateway(
-    'internetGateway'
-))
-
-vpcInternetGatewayAttachment = template.add_resource(ec2.VPCGatewayAttachment(
-    'vpcInternetGateway',
-    InternetGatewayId=Ref(internetGateway),
-    VpcId=Ref(vpc)
-))
-
 subnetPub = template.add_resource(ec2.Subnet(
     'subnetPub',
     CidrBlock=cidr_subnet_pub,
@@ -92,100 +70,6 @@ template.add_resource(ec2.SubnetRouteTableAssociation(
     'pubSubnetRouteTableAssociation',
     RouteTableId=Ref(subnetPubRouteTable),
     SubnetId=Ref(subnetPub)
-))
-
-template.add_resource(ec2.Route(
-    'subletPrivInstanceNatRoute',
-    RouteTableId=Ref(subnetPubRouteTable),
-    DestinationCidrBlock='0.0.0.0/0',
-    GatewayId=Ref(internetGateway),
-    DependsOn=vpcInternetGatewayAttachment.title
-))
-
-
-subnetPriv = template.add_resource(ec2.Subnet(
-    'subnetPriv',
-    CidrBlock=cidr_subnet_priv,
-    VpcId=Ref(vpc),
-    Tags=Tags(
-        Name='subnetPriv'
-    )
-))
-
-
-subnetPrivRouteTable = template.add_resource(ec2.RouteTable(
-    'subnetPrivRouteTable',
-    VpcId=Ref(vpc),
-    Tags=Tags(
-        VPC=Ref(vpcName)
-    )
-))
-
-
-template.add_resource(ec2.SubnetRouteTableAssociation(
-    'subnetPugRouteTableAssociation',
-    RouteTableId=Ref(subnetPrivRouteTable),
-    SubnetId=Ref(subnetPriv)
-))
-
-
-instanceNAT = template.add_resource(ec2.Instance(
-    'instanceNAT',
-    ImageId='ami-7da94839',
-    InstanceType='t2.micro',
-    KeyName=Ref(natKeyName),
-    SourceDestCheck='false',
-    NetworkInterfaces=[
-        ec2.NetworkInterfaceProperty(
-            'natNetworkInterface',
-            Description='instaneNAT network interface',
-            SubnetId=Ref(subnetPub),
-            DeviceIndex=0,
-            PrivateIpAddresses=[
-                ec2.PrivateIpAddressSpecification(
-                    'natPrivateIp',
-                    Primary='true',
-                    PrivateIpAddress=Ref(natIP),
-                )
-            ],
-            GroupSet=[Ref(vpcSecurityGroupDefault)]
-        )
-    ]
-))
-
-instancePrivate = template.add_resource(ec2.Instance(
-    'instancePrivate',
-    ImageId='ami-de347abe',
-    InstanceType='t2.micro',
-    KeyName=Ref(natKeyName),
-    NetworkInterfaces=[
-        ec2.NetworkInterfaceProperty(
-            'natNetworkInterfaceInstancePrivate',
-            Description='private insatnce network interface',
-            SubnetId=Ref(subnetPriv),
-            DeviceIndex=0,
-            GroupSet=[Ref(vpcSecurityGroupDefault)]
-        )
-    ]
-))
-
-instanceNATEIP = template.add_resource(ec2.EIP(
-    'instanceNATEIP',
-    DependsOn=vpcInternetGatewayAttachment.title
-))
-
-instanceNATEIPAssociation = template.add_resource(ec2.EIPAssociation(
-    'instanceNATEIPAssociation',
-    AllocationId=GetAtt(instanceNATEIP, 'AllocationId'),
-    InstanceId=Ref(instanceNAT)
-))
-
-template.add_resource(ec2.Route(
-    'subletPrivInatanceNateRoute',
-    RouteTableId=Ref(subnetPrivRouteTable),
-    DestinationCidrBlock='0.0.0.0/0',
-    InstanceId=Ref(instanceNAT),
-    DependsOn=instanceNAT.title
 ))
 
 print(template.to_json())
