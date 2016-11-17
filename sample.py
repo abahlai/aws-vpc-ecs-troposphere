3349225072b864c8c4a9a750dfacdab20181564b
nikotinik <nikotinik@hotmail.com>
2016-11-17 22:10:56 +0200

    new version of sample.py

--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@ from troposphere import (
     AWS_REGION,
     AWS_STACK_ID,
     AWS_STACK_NAME,
+	AWS_ACCOUNT_ID,
     autoscaling,
     Base64,
     cloudformation,
@@ -12,10 +13,15 @@ from troposphere import (
     Parameter,
     Ref,
     GetAtt,
+	elasticloadbalancing as elb,
+	Output,
+	Not,
+	Equals,
 )
 
 from troposphere.ec2 import (
     EIP,
+	Instance,
     InternetGateway,
     NatGateway,
     Route,
@@ -30,12 +36,84 @@ from troposphere.ec2 import (
 
 from troposphere.ecs import (
     Cluster,
+    ContainerDefinition,
+    Environment,
+    LoadBalancer,
+    LogConfiguration,
+    PortMapping,
+    Service,
+    TaskDefinition,
 )
+
+from troposphere.certificatemanager import (
+    Certificate,
+    DomainValidationOption,
+)
+
+from awacs import ecr
+
+from awacs.aws import (
+    Allow,
+    Policy,
+    AWSPrincipal,
+    Statement,
+)
+
+from troposphere.ecr import Repository
 
 # The CloudFormation template
 template = Template()
 
-#==========VPC Section==========//
+# Repository
+
+repository = Repository(
+    "ApplicationRepository",
+    template=template,
+    RepositoryName="application",
+    # Allow all account users to manage images.
+    RepositoryPolicyText=Policy(
+        Version="2008-10-17",
+        Statement=[
+            Statement(
+                Sid="AllowPushPull",
+                Effect=Allow,
+                Principal=AWSPrincipal([
+                    Join("", [
+                        "arn:aws:iam::",
+                        Ref(AWS_ACCOUNT_ID),
+                        ":root",
+                    ]),
+                ]),
+                Action=[
+                    ecr.GetDownloadUrlForLayer,
+                    ecr.BatchGetImage,
+                    ecr.BatchCheckLayerAvailability,
+                    ecr.PutImage,
+                    ecr.InitiateLayerUpload,
+                    ecr.UploadLayerPart,
+                    ecr.CompleteLayerUpload,
+                ],
+            ),
+        ]
+    ),
+)
+
+
+# Output ECR repository URL
+template.add_output(Output(
+    "RepositoryURL",
+    Description="The docker repository URL",
+    Value=Join("", [
+        Ref(AWS_ACCOUNT_ID),
+        ".dkr.ecr.",
+        Ref(AWS_REGION),
+        ".amazonaws.com/",
+        Ref(repository),
+    ]),
+))
+
+# VPC
+
 vpc = VPC(
     "Vpc",
     template=template,
@@ -154,7 +232,6 @@ private_nat_route = Route(
     InstanceId=Ref("Nat"),
 )
 
-
 # Holds containers instances
 container_a_subnet_cidr = "10.0.10.0/24"
 container_a_subnet = Subnet(
@@ -191,7 +268,69 @@ SubnetRouteTableAssociation(
     RouteTableId=Ref(private_route_table),
 )
 
-#==========Container Section==========//
+# Cluster
+
+container_instance_type = Ref(template.add_parameter(Parameter(
+    "ContainerInstanceType",
+    Description="The container instance type",
+    Type="String",
+    Default="t2.micro",
+    AllowedValues=["t2.micro", "t2.small", "t2.medium"]
+)))
+
+web_worker_cpu = Ref(template.add_parameter(Parameter(
+	"WebWorkerCPU",
+    Description="Web worker CPU units",
+    Type="Number",
+    Default="512",
+)))
+
+
+web_worker_memory = Ref(template.add_parameter(Parameter(
+    "WebWorkerMemory",
+    Description="Web worker memory",
+    Type="Number",
+    Default="700",
+)))
+
+
+web_worker_desired_count = Ref(template.add_parameter(Parameter(
+    "WebWorkerDesiredCount",
+    Description="Web worker task instance count",
+    Type="Number",
+    Default="2",
+)))
+
+max_container_instances = Ref(template.add_parameter(Parameter(
+    "MaxScale",
+    Description="Maximum container instances count",
+    Type="Number",
+    Default="3",
+)))
+
+desired_container_instances = Ref(template.add_parameter(Parameter(
+    "DesiredScale",
+    Description="Desired container instances count",
+    Type="Number",
+    Default="3",
+)))
+
+app_revision = Ref(template.add_parameter(Parameter(
+    "WebAppRevision",
+    Description="An optional docker app revision to deploy",
+    Type="String",
+    Default="",
+)))
+
+
+deploy_condition = "Deploy"
+template.add_condition(deploy_condition, Not(Equals(app_revision, "")))
+
+template.add_mapping("ECSRegionMap", {
+    "eu-west-1": {"AMI": "ami-4e6ffe3d"},
+    "us-east-1": {"AMI": "ami-8f7687e2"},
+    "us-west-2": {"AMI": "ami-84b44de4"},
+})
 
 # ECS cluster
 cluster = Cluster(
@@ -210,6 +349,31 @@ container_instance_role = iam.Role(
     )]),
     Path="/",
     Policies=[
+        # iam.Policy(
+            # PolicyName="AssetsManagementPolicy",
+            # PolicyDocument=dict(
+                # Statement=[dict(
+                    # Effect="Allow",
+                    # Action=[
+                        # "s3:ListBucket",
+                    # ],
+                    # Resource=Join("", [
+                        # "arn:aws:s3:::",
+                        # Ref(assets_bucket),
+                    # ]),
+                # ), dict(
+                    # Effect="Allow",
+                    # Action=[
+                        # "s3:*",
+                    # ],
+                    # Resource=Join("", [
+                        # "arn:aws:s3:::",
+                        # Ref(assets_bucket),
+                        # "/*",
+                    # ]),
+                # )],
+            # ),
+        # ),
         iam.Policy(
             PolicyName="ECSManagementPolicy",
             PolicyDocument=dict(
@@ -223,9 +387,36 @@ container_instance_role = iam.Role(
                 )],
             ),
         ),
+        iam.Policy(
+            PolicyName='ECRManagementPolicy',
+            PolicyDocument=dict(
+                Statement=[dict(
+                    Effect='Allow',
+                    Action=[
+                        ecr.GetAuthorizationToken,
+                        ecr.GetDownloadUrlForLayer,
+                        ecr.BatchGetImage,
+                        ecr.BatchCheckLayerAvailability,
+                    ],
+                    Resource="*",
+                )],
+            ),
+        ),
+        # iam.Policy(
+            # PolicyName="LoggingPolicy",
+            # PolicyDocument=dict(
+                # Statement=[dict(
+                    # Effect="Allow",
+                    # Action=[
+                        # "logs:Create*",
+                        # "logs:PutLogEvents",
+                    # ],
+                    # Resource="arn:aws:logs:*:*:*",
+                # )],
+            # ),
+        # ),
     ]
 )
-
 
 # ECS container instance profile
 container_instance_profile = iam.InstanceProfile(
@@ -235,26 +426,10 @@ container_instance_profile = iam.InstanceProfile(
     Roles=[Ref(container_instance_role)],
 )
 
-#==========AutoScaling LaunchConf==========//
-
-container_instance_type = Ref(template.add_parameter(Parameter(
-    "ContainerInstanceType",
-    Description="The container instance type",
-    Type="String",
-    Default="t2.micro",
-    AllowedValues=["t2.micro", "t2.small", "t2.medium"]
-)))
-
-
-template.add_mapping("ECSRegionMap", {
-    "eu-west-1": {"AMI": "ami-4e6ffe3d"},
-    "us-east-1": {"AMI": "ami-8f7687e2"},
-    "us-west-2": {"AMI": "ami-84b44de4"},
-})
-
-
 container_instance_configuration_name = "ContainerLaunchConfiguration"
 
+
+autoscaling_group_name = "AutoScalingGroup"
 
 container_instance_configuration = autoscaling.LaunchConfiguration(
     container_instance_configuration_name,
@@ -268,7 +443,11 @@ container_instance_configuration = autoscaling.LaunchConfiguration(
                         # Register the cluster
                         "echo ECS_CLUSTER=",
                         Ref(cluster),
-                        " >> /etc/ecs/config\n",
+                        " >> /etc/ecs/ecs.config\n",
+                        # Enable CloudWatch docker logging
+                        'echo \'ECS_AVAILABLE_LOGGING_DRIVERS=',
+                        '["json-file","awslogs"]\'',
+                        " >> /etc/ecs/ecs.config\n",
                     ]))
                 ),
                 files=cloudformation.InitFiles({
@@ -295,7 +474,7 @@ container_instance_configuration = autoscaling.LaunchConfiguration(
                             % container_instance_configuration_name,
                             "Metadata.AWS::CloudFormation::Init\n",
                             "action=/opt/aws/bin/cfn-init -v ",
-                            "         --template ",
+                            "         --stack",
                             Ref(AWS_STACK_NAME),
                             "         --resource %s"
                             % container_instance_configuration_name,
@@ -330,30 +509,11 @@ container_instance_configuration = autoscaling.LaunchConfiguration(
         "yum install -y aws-cfn-bootstrap\n",
 
         "/opt/aws/bin/cfn-init -v ",
-        "         --template ", Ref(AWS_STACK_NAME),
+        "         --stack", Ref(AWS_STACK_NAME),
         "         --resource %s " % container_instance_configuration_name,
         "         --region ", Ref(AWS_REGION), "\n",
     ])),
 )
-
-#==========AutoScaling Section==========//
-
-max_container_instances = Ref(template.add_parameter(Parameter(
-    "MaxScale",
-    Description="Maximum container instances count",
-    Type="Number",
-    Default="3",
-)))
-
-desired_container_instances = Ref(template.add_parameter(Parameter(
-    "DesiredScale",
-    Description="Desired container instances count",
-    Type="Number",
-    Default="3",
-)))
-
-
-autoscaling_group_name = "AutoScalingGroup"
 
 
 autoscaling_group = autoscaling.AutoScalingGroup(
@@ -371,6 +531,124 @@ autoscaling_group = autoscaling.AutoScalingGroup(
     # instance will be flagged as `unhealthy` and won't stop respawning'
     HealthCheckType="EC2",
     HealthCheckGracePeriod=300,
+)
+
+# ECS task
+web_task_definition = TaskDefinition(
+    "WebTask",
+    template=template,
+    Condition=deploy_condition,
+    ContainerDefinitions=[
+        ContainerDefinition(
+            Name="WebWorker",
+            #  1024 is full CPU
+            Cpu=web_worker_cpu,
+            Memory=web_worker_memory,
+            Essential=True,
+            Image=Join("", [
+				Ref(AWS_ACCOUNT_ID),
+                ".dkr.ecr.",
+                Ref(AWS_REGION),
+                ".amazonaws.com/",
+                Ref(repository),
+                ":",
+                app_revision,
+            ]),
+            # PortMappings=[PortMapping(
+                # ContainerPort=web_worker_port,
+                # HostPort=web_worker_port,
+            # )],
+            # LogConfiguration=LogConfiguration(
+                # LogDriver="awslogs",
+                # Options={
+                    # 'awslogs-group': Ref(web_log_group),
+                    # 'awslogs-region': Ref(AWS_REGION),
+                # }
+            # ),
+            # Environment=[
+                # Environment(
+                    # Name="AWS_STORAGE_BUCKET_NAME",
+                    # Value=Ref(assets_bucket),
+                # ),
+                # Environment(
+                    # Name="CDN_DOMAIN_NAME",
+                    # Value=GetAtt(distribution, "DomainName"),
+                # ),
+                # Environment(
+                    # Name="DOMAIN_NAME",
+                    # Value=domain_name,
+                # ),
+                # Environment(
+                    # Name="PORT",
+                    # Value=web_worker_port,
+                # ),
+                # Environment(
+                    # Name="SECRET_KEY",
+                    # Value=secret_key,
+                # ),
+                # Environment(
+                    # Name="DATABASE_URL",
+                    # Value=Join("", [
+                        # "postgres://",
+                        # Ref(db_user),
+                        # ":",
+                        # Ref(db_password),
+                        # "@",
+                        # GetAtt(db_instance, 'Endpoint.Address'),
+                        # "/",
+                        # Ref(db_name),
+                    # ]),
+                # ),
+            # ],
+        )
+    ],
+)
+
+app_service_role = iam.Role(
+    "AppServiceRole",
+    template=template,
+    AssumeRolePolicyDocument=dict(Statement=[dict(
+        Effect="Allow",
+        Principal=dict(Service=["ecs.amazonaws.com"]),
+        Action=["sts:AssumeRole"],
+    )]),
+    Path="/",
+    Policies=[
+        iam.Policy(
+            PolicyName="WebServicePolicy",
+            PolicyDocument=dict(
+                Statement=[dict(
+                    Effect="Allow",
+                    Action=[
+                        "elasticloadbalancing:Describe*",
+                        "elasticloadbalancing"
+                        ":DeregisterInstancesFromLoadBalancer",
+                        "elasticloadbalancing"
+                        ":RegisterInstancesWithLoadBalancer",
+                        "ec2:Describe*",
+                        "ec2:AuthorizeSecurityGroupIngress",
+                    ],
+                    Resource="*",
+                )],
+            ),
+        ),
+    ]
+)
+
+app_service = Service(
+    "AppService",
+    template=template,
+    Cluster=Ref(cluster),
+    Condition=deploy_condition,
+    DependsOn=[autoscaling_group_name],
+    DesiredCount=web_worker_desired_count,
+    # LoadBalancers=[LoadBalancer(
+        # ContainerName="WebWorker",
+        # ContainerPort=web_worker_port,
+        # LoadBalancerName=Ref(load_balancer),
+    # )],
+    TaskDefinition=Ref(web_task_definition),
+    Role=Ref(app_service_role),
 )
 
 print(template.to_json())
